13355b8ff33cc2724ef802c3e832cbed80358c59
Nate <nateclarke1500@gmail.com>
2022-05-07 22:21:35 -0700

    Make project

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use read_input::prelude::*;
+
+fn main(){
+
+    println!("Do you want to enter the equations manually or randomly?");
+
+
+}
+
+
+fn man_input(){
+
+}
+
+
+fn rand_input(){
+
+}
+
+
+fn jacobi(){
+
+}
+
+
+fn gauss(){
+
+}
