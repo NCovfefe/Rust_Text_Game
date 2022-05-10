deb4cbd438d7863896415c7214d6cbe385368c27
NCovfefe <71348856+NCovfefe@users.noreply.github.com>
2022-05-09 20:00:27 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,7 @@ use read_input::prelude::*;
 
 fn main(){
 
-    println!("Do you want to enter the equations manually or randomly?");
+   
 
-
-}
-
-
-fn man_input(){
-
-}
-
-
-fn rand_input(){
-
-}
-
-
-fn jacobi(){
-
-}
-
-
-fn gauss(){
 
 }
